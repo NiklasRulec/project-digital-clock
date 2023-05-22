759d4f1c82a45fd16a14ab653305f8cf5b5bbc51
NiklasRulec <niklasrulec.98@gmail.com>
2023-05-22 15:14:19 +0200

    fixed

--- main.js
+++ main.js
@@ -25,20 +25,6 @@ let wochenTag = [
 
 // ! ++++++++++
 
-let date1 = new Date();
-document.write(date1 + "<br>");
-document.write(date1.getFullYear() + "<br>");
-document.write(date1.getMonth() + " Monat" + "<br>");
-document.write(date1.getDay() + " Tag" + "<br>");
-document.write(date1.getHours() + " Stunde" + "<br>");
-document.write(date1.getMinutes() + " Minute" + "<br>");
-let weekday = date1.toLocaleString("default", { weekday: "long" });
-document.write(weekday + "<br>");
-let month = date1.toLocaleString("default", { month: "long" });
-document.write(month + "<br>");
-
-// ! ++++++++++
-
 const day = document.querySelector(".day");
 const hour = document.querySelector(".hour");
 const minute = document.querySelector(".minute");
@@ -46,20 +32,17 @@ const second = document.querySelector(".second");
 const period = document.querySelector(".period");
 
 function clock() {
-  let weekday2 = date1.toLocaleString("default", { weekday: "short" });
-  day.innerHTML = weekday2;
-  hour.innerHTML = date1.getHours();
-  minute.innerHTML = date1.getMinutes();
-  second.innerHTML = date1.getSeconds();
-  if (date1.getHours() < 12) {
+  let date = new Date();
+  let weekday = date.toLocaleString("default", { weekday: "short" });
+  day.innerHTML = weekday;
+  hour.innerHTML = date.getHours();
+  minute.innerHTML = date.getMinutes();
+  second.innerHTML = date.getSeconds();
+  if (date.getHours() < 12) {
     period.innerHTML = "AM";
   } else {
     period.innerHTML = "PM";
   }
 }
 
-clock();
-
-setInterval(function () {
-  location.reload();
-}, 1000);
+setInterval(clock, 500);
